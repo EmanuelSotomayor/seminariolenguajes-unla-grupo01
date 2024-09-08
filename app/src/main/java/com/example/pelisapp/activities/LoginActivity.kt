67c7ea1fe606b745aa6ce80a8b8894a6bfe2479f
Emanuel Sotomayor <emanuel.sotomayor.sec@gmail.com>
2024-09-08 01:20:14 -0300

    Se agrega Intent y eventos para botones y se cambia el filtro de intent para pantalla principal

--- app/src/main/java/com/example/pelisapp/activities/LoginActivity.kt
+++ app/src/main/java/com/example/pelisapp/activities/LoginActivity.kt
@@ -2,6 +2,10 @@ package com.example.pelisapp.activities
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.CheckBox
+import android.widget.EditText
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,7 +15,14 @@ import com.example.pelisapp.R
 
 class LoginActivity : AppCompatActivity() {
 
+    private lateinit var loginBtn: Button;
+    private lateinit var signUpBtn: Button;
+    private lateinit var inputEmail: EditText;
+    private lateinit var inputPassword: EditText;
+    private lateinit var checkboxRememberUser: CheckBox;
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_login)
@@ -20,6 +31,30 @@ class LoginActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        this.loginBtn = findViewById(R.id.btnLogin);
+        this.signUpBtn = findViewById(R.id.btnSignUp);
+        this.inputEmail = findViewById(R.id.inputEmail);
+        this.inputPassword = findViewById(R.id.inputPassword);
+        this.checkboxRememberUser = findViewById(R.id.checkBoxRememberUser);
+
+        this.loginBtn.setOnClickListener {
+            val emailValue = this.inputEmail.text.toString();
+            val passwordValue = this.inputPassword.text.toString();
+                if(emailValue.isEmpty() || passwordValue.isEmpty()) {
+                    Toast.makeText(this, "The inputs can't be empty", Toast.LENGTH_SHORT).show()
+                }else{
+                    if(this.checkboxRememberUser.isChecked){
+                        Toast.makeText(this, "${emailValue} Remembered email", Toast.LENGTH_SHORT).show()
+                    }
+                    this.navigateToMainActivity();
+                }
+        }
+
+        this.signUpBtn.setOnClickListener{
+            //this.navigateToRegisterActivity();
+        }
+
     }
 
     private fun navigateToMainActivity(): Unit{
@@ -30,6 +65,11 @@ class LoginActivity : AppCompatActivity() {
     private fun navigateToRegisterActivity(): Unit{
         //val intentNavigateToRegister = Intent(this, RegisterActivity::class.java);
         //startActivity(intentNavigateToRegister);
+    }
+
+    private fun navigateToForgotPasswordActivity(): Unit{
+        //val intentNavigateToForgotPassword = Intent(this, ForgotPasswordActivity::class.java);
+        //startActivity(intentNavigateToForgotPassword);
     }
 
 }
