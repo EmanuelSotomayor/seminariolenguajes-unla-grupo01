d508d33ab1f3c3254cb1811b2976215e8ec82b8e
Ana vazquez <anav4332816@gmail.com>
2024-09-08 22:42:22 -0300

    Crear view de cuenta

--- app/src/main/java/com/example/pelisapp/MainActivity.kt
+++ app/src/main/java/com/example/pelisapp/MainActivity.kt
@@ -1,20 +1,38 @@
 package com.example.pelisapp
 
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+    lateinit var emailInput : EditText
+    lateinit var contraseñaInput : EditText
+    lateinit var confContraseñaInput : EditText
+    lateinit var registrarBtn : Button
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        emailInput = findViewById(R.id.editEmail)
+        contraseñaInput = findViewById(R.id.editContraseña)
+        confContraseñaInput = findViewById(R.id.editConfContraseña)
+        registrarBtn = findViewById(R.id.btnRegistro)
+
+
+        registrarBtn.setOnClickListener {
+            val email = emailInput.text.toString()
+            val contraseña = contraseñaInput.text.toString()
+            val confContraseña = confContraseñaInput.text.toString()
+            Log.i("Cuentas creadas --> ", "Email : $email , contraseña : $contraseña y confirmacion : $confContraseña")
+
         }
     }
 }
