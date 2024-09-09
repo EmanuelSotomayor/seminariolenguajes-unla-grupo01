ab653fb620deb6e6fd813a25b9ab8ec3617aa26c
Nehuen2002 <nehuengarcia2002@gmail.com>
2024-09-09 12:29:32 -0300

    refactor detail

--- app/src/main/java/com/example/pelisapp/favorite/DetailActivity.kt
+++ app/src/main/java/com/example/pelisapp/favorite/DetailActivity.kt
@@ -1,10 +1,11 @@
-package com.example.pelisapp
+package com.example.pelisapp.favorite
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.pelisapp.R
 
 class DetailActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
