1599dfa22fba2fb30c2f2916776c24d4f86f6ae0
Ana vazquez <anav4332816@gmail.com>
2024-09-08 23:45:39 -0300

    Crear view de cuenta

--- app/src/main/java/com/example/pelisapp/createaccounts/CreateAccountsActivity.kt
+++ app/src/main/java/com/example/pelisapp/createaccounts/CreateAccountsActivity.kt
@@ -1,6 +1,9 @@
 package com.example.pelisapp.createaccounts
 
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -8,14 +11,29 @@ import androidx.core.view.WindowInsetsCompat
 import com.example.pelisapp.R
 
 class CreateAccountsActivity : AppCompatActivity() {
+    lateinit var emailInput : EditText
+    lateinit var contraseñaInput : EditText
+    lateinit var confContraseñaInput : EditText
+    lateinit var registrarBtn : Button
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_create_accounts)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        setContentView(R.layout.activity_main)
+
+        emailInput = findViewById(R.id.editEmail)
+        contraseñaInput = findViewById(R.id.editContraseña)
+        confContraseñaInput = findViewById(R.id.editConfContraseña)
+        registrarBtn = findViewById(R.id.btnRegistro)
+
+
+        registrarBtn.setOnClickListener {
+            val email = emailInput.text.toString()
+            val contraseña = contraseñaInput.text.toString()
+            val confContraseña = confContraseñaInput.text.toString()
+            Log.i("Cuentas creadas --> ", "Email : $email , contraseña : $contraseña y confirmacion : $confContraseña")
+
         }
     }
 }
