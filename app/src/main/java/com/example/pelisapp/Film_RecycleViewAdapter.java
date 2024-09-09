f4a928f06745435ee1a07e1b15b724aa826f69f0
lucianomp9 <lucianoperez12@gmail.com>
2024-09-08 22:34:21 -0300

    Add home menu view

---
+++ app/src/main/java/com/example/pelisapp/Film_RecycleViewAdapter.java
@@ -0,0 +1,58 @@
+package com.example.pelisapp;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class Film_RecycleViewAdapter extends RecyclerView.Adapter<Film_RecycleViewAdapter.MyViewHolder>{
+
+    Context context;
+    ArrayList<FilmModel> filmModels;
+
+    public Film_RecycleViewAdapter(Context context, ArrayList<FilmModel> filmModels){
+        this.context = context;
+        this.filmModels = filmModels;
+    }
+
+    @NonNull
+    @Override
+    public Film_RecycleViewAdapter.MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater inflater = LayoutInflater.from(context);
+        View view = inflater.inflate(R.layout.recycler_view_row, parent, false);
+        return new Film_RecycleViewAdapter.MyViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull Film_RecycleViewAdapter.MyViewHolder holder, int position) {
+        holder.filmName.setText(filmModels.get(position).getTitle());
+        holder.filmInfo.setText(filmModels.get(position).getInfo());
+        holder.imageView.setImageResource(filmModels.get(position).getImage());
+    }
+
+    @Override
+    public int getItemCount() {
+        return filmModels.size();
+    }
+
+    public static class MyViewHolder extends RecyclerView.ViewHolder{
+
+        ImageView imageView;
+        TextView filmName, filmInfo;
+
+        public MyViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            imageView = itemView.findViewById(R.id.imageView);
+            filmName = itemView.findViewById(R.id.filmName);
+            filmInfo = itemView.findViewById(R.id.filmDescription);
+        }
+    }
+}
