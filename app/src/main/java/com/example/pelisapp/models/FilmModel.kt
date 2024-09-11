88fcb555dffb78e8bcfca4b285d34f22f8f19826
lucianomp9 <lucianoperez12@gmail.com>
2024-09-10 21:54:43 -0300

    Modify file type for home view

---
+++ app/src/main/java/com/example/pelisapp/models/FilmModel.kt
@@ -0,0 +1,7 @@
+package com.example.pelisapp.models
+
+data class FilmModel(
+        val image: Int,
+        val title: String,
+        val info: String
+)
---
+++ app/src/main/java/com/example/pelisapp/views/adapters/FilmRecyclerViewAdapter.kt
@@ -0,0 +1,51 @@
+package com.example.pelisapp.views.adapters
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.pelisapp.R
+import com.example.pelisapp.models.FilmModel
+
+class FilmRecyclerViewAdapter(
+    private val context: Context,
+    private val filmModels: ArrayList<FilmModel>
+) : RecyclerView.Adapter<FilmRecyclerViewAdapter.MyViewHolder>(), View.OnClickListener {
+
+    private var listener: View.OnClickListener? = null
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+        val inflater = LayoutInflater.from(context)
+        val view = inflater.inflate(R.layout.recycler_view_row, parent, false)
+        view.setOnClickListener(this)
+        return MyViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+        val film = filmModels[position]
+        holder.filmName.text = film.title
+        holder.filmInfo.text = film.info
+        holder.imageView.setImageResource(film.image)
+    }
+
+    override fun getItemCount(): Int {
+        return filmModels.size
+    }
+
+    fun setOnClickListener(listener: View.OnClickListener) {
+        this.listener = listener
+    }
+
+    override fun onClick(view: View) {
+        listener?.onClick(view)
+    }
+
+    class MyViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val imageView: ImageView = itemView.findViewById(R.id.imageView)
+        val filmName: TextView = itemView.findViewById(R.id.filmName)
+        val filmInfo: TextView = itemView.findViewById(R.id.filmDescription)
+    }
+}
