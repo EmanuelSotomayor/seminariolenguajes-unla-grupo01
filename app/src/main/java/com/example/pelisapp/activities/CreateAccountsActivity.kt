57fd4f84bf605d4f9b7d79c164f983b5cf774975
Ana vazquez <anav4332816@gmail.com>
2024-09-30 13:20:59 -0300

    Validaciones para registrar un usuario.

--- app/src/main/java/com/example/pelisapp/activities/CreateAccountsActivity.kt
+++ app/src/main/java/com/example/pelisapp/activities/CreateAccountsActivity.kt
@@ -7,6 +7,7 @@ import android.util.Log
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
@@ -15,7 +16,6 @@ import com.example.pelisapp.R
 import com.example.pelisapp.database.model.UserViewModel
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.launch
-
 
 @AndroidEntryPoint
 class CreateAccountsActivity : AppCompatActivity() {
@@ -29,6 +29,12 @@ class CreateAccountsActivity : AppCompatActivity() {
     @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        if(isUserRegistered()){
+            val intent = Intent(this@CreateAccountsActivity,HomeMenuActivity::class.java)
+            startActivity(intent)
+            finish()
+            return
+        }
         enableEdgeToEdge()
         setContentView(R.layout.activity_create_accounts)
 
@@ -41,21 +47,81 @@ class CreateAccountsActivity : AppCompatActivity() {
 
 
         registrarBtn.setOnClickListener {
+
             val email = emailInput.text.toString()
             val contraseña = contraseñaInput.text.toString()
             val confContraseña = confContraseñaInput.text.toString()
-            //val user = UserEntity(name = "juan", email = email, password = contraseña)
-            lifecycleScope.launch {
+            //val nombre = nombreInput.text.toString()
 
-             //userViewModel.addUser(user)
+            //val user = UserEntity(email = email, contraseña = contraseña,confContraseña = confContraseña);
+          if (validarCampos(email,contraseña,confContraseña)){
+            lifecycleScope.launch {
+                if(userExits(email)){
+                    emailInput.error = "Este email ya esta registrado"
+                }else {
+                    val user = UserEntity(email = email,contraseña = contraseña,confContraseña = confContraseña)
+                    userViewModel.addUser(user)
+                    val userId = userViewModel.getUserByEmail(email)
+                    val sharedPreferences = getSharedPreferences("user_session", MODE_PRIVATE)
+                    val editor = sharedPreferences.edit()
+                    editor.putString("email",email)
+                    editor.putString("password",contraseña)
+                    editor.putInt("userId",userId?.userId ?: -1)
+                    editor.putBoolean("isRegistered",true)
+                    editor.apply()
+
+                    val intent = Intent(this@CreateAccountsActivity,HomeMenuActivity::class.java)
+                    startActivity(intent)
+                    finish()
+                }
             }
-            val intent = Intent(this, HomeMenuActivity::class.java)
-            startActivity(intent)
+          }
         }
+
         ingresarBtn.setOnClickListener {
             val intent = Intent(this, LoginActivity::class.java)
             startActivity(intent)
         }
+
+    }
+
+    private fun validarCampos(email: String,contraseña: String,confContraseña: String):Boolean{
+        val emailPattern = "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+"
+        if (email.isEmpty()) {
+            emailInput.error = "El email no puede estar vacío"
+            return false
+        } else if (!email.matches(emailPattern.toRegex())) {
+            emailInput.error = "Ingrese un correo electrónico válido"
+            return false
+        }
+
+        if(contraseña.isEmpty()){
+            contraseñaInput.error = "La contraseña no puede estar vacia"
+            return false
+        }else if(contraseña.length < 6){
+            contraseñaInput.error = "La contraseña debe tener al menos 6 caracteres"
+            return false
+        }
+
+        if(confContraseña.isEmpty()){
+            confContraseñaInput.error = "Debe confirmar contraseña"
+            return false
+        }else if (contraseña != confContraseña){
+            confContraseñaInput.error = "Las contraseñas no son iguales"
+            return false
+        }
+        //Si pasa todas las validaciones,devuelve true
+        return true
+    }
+
+    private suspend fun userExits(email: String): Boolean{
+        val user = userViewModel.userExists(email)
+        return user
+    }
+
+    private fun isUserRegistered(): Boolean {
+        val sharedPreferences = getSharedPreferences("user_session", MODE_PRIVATE)
+        return sharedPreferences.getBoolean("isRegistred",false)
     }
 
 }
