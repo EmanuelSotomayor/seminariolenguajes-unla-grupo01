4cd625ac7849b77cbdaaa8363e65a15fc9b87436
christopher cruz <christophercruz04@hotmail.com>
2024-10-09 04:29:01 -0300

    corrigiendo but

--- app/src/main/java/com/example/pelisapp/activities/FavoriteActivity.kt
+++ app/src/main/java/com/example/pelisapp/activities/FavoriteActivity.kt
@@ -6,6 +6,7 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.pelisapp.R
 import com.example.pelisapp.databinding.ActivityFavoriteBinding
 import com.example.pelisapp.models.FavoriteFilmModel
@@ -14,23 +15,25 @@ import com.example.pelisapp.views.adapters.FavoriteFilmAdapter
 
 class FavoriteActivity : BaseActivity() {
     private lateinit var binding: ActivityFavoriteBinding
+
     private var favoriteMoviesMutableList: MutableList<FavoriteFilmModel> = FavoriteFilmProvider.favoriteMoviesList.toMutableList()
     private lateinit var adapter: FavoriteFilmAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        layoutInflater.inflate(R.layout.activity_favorite, findViewById(R.id.activity_content))
-       binding= ActivityFavoriteBinding.inflate(layoutInflater)
+        binding= ActivityFavoriteBinding.inflate(layoutInflater)
         //setContentView(binding.root)
-        initRecyclerView()
+        layoutInflater.inflate(R.layout.activity_favorite, findViewById(R.id.activity_content))
+        val recyclerView: RecyclerView=findViewById(R.id.recycleFavoritePeli)
+        initRecyclerView(recyclerView)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
     }
-   private fun initRecyclerView(){
+   private fun initRecyclerView(recyclerView: RecyclerView){
 
         /*val recyclerView = findViewById<RecyclerView>(R.id.recycleFavoritePeli)
         recyclerView.layoutManager = LinearLayoutManager(this)
@@ -41,8 +44,10 @@ class FavoriteActivity : BaseActivity() {
            onClickDelete = {position -> onDeleteItem(position)}
        )
 
-       binding.recycleFavoritePeli.layoutManager = LinearLayoutManager(this)
-       binding.recycleFavoritePeli.adapter =adapter
+      /* binding.recycleFavoritePeli.layoutManager = LinearLayoutManager(this)
+       binding.recycleFavoritePeli.adapter =adapter*/
+       recyclerView.layoutManager = LinearLayoutManager(this)
+       recyclerView.adapter = adapter
    }
     private fun onDeleteItem(position: Int) {
         favoriteMoviesMutableList.removeAt(position)
