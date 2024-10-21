e5cb6df7f7ed485a0506d4dc748f4a71ee44e7aa
Ana vazquez <anav4332816@gmail.com>
2024-10-21 15:12:55 -0300

    Uso de fragmentos en una de las pantallas,tercer entrega.

--- app/src/main/java/com/example/pelisapp/activities/DetailActivity.kt
+++ app/src/main/java/com/example/pelisapp/activities/DetailActivity.kt
@@ -1,49 +1,113 @@
 package com.example.pelisapp.activities
 
+import android.content.Intent
+import android.content.SharedPreferences
 import android.os.Bundle
 import android.util.Log
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.lifecycle.lifecycleScope
+import coil.load
 import com.example.pelisapp.BottomFragment
 import com.example.pelisapp.R
 import com.example.pelisapp.TopFragment
-import com.example.pelisapp.models.FilmModel
+import com.example.pelisapp.data.MovieApiViewModel
+import com.example.pelisapp.data.MovieDetailDataModel
+import com.example.pelisapp.database.entities.FavoriteMovieEntity
+import com.example.pelisapp.database.model.UserRepositoryViewModel
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
 
+@AndroidEntryPoint
 class DetailActivity : AppCompatActivity() {
+    private lateinit var button2: Button
+    private lateinit var titleView: TextView
+    private lateinit var descriptionView: TextView
+    private lateinit var imageView: ImageView
+    private lateinit var sharedPreferences2: SharedPreferences
+    private val userRepositoryViewModel: UserRepositoryViewModel by viewModels()
+    private val movieApiViewModel: MovieApiViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_detail)
 
+        titleView = findViewById(R.id.titleView)
+        descriptionView = findViewById(R.id.descriptionView)
+        imageView = findViewById(R.id.imageView)
+        button2 = findViewById(R.id.button2)
+        sharedPreferences2 = getSharedPreferences("user_session", MODE_PRIVATE)
+
+        // Obtener el ID de la película que se pasó como extra
+        val movieId = intent.getIntExtra("movie_id", -1)
+        if (movieId != -1) {
+            getMovieDetails(movieId)
+        } else {
+            Log.e("DetailActivity", "Movie ID is invalid!")
+        }
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+    }
 
-        // Obtiene el objeto FilmModel desde el Intent
-       val film = intent.getParcelableExtra<FilmModel>("film")
-       if (film != null){
-           // Carga el TopFragment con título e imagen usando newInstance
-           val topFragment = TopFragment.newInstance(film.title, film.image)
+    private fun getMovieDetails(movieId: Int) {
+        lifecycleScope.launch {
+            try {
+                val movieDetail = movieApiViewModel.getDetailMovieById(movieId)
+                titleView.text = movieDetail.title
+                descriptionView.text = movieDetail.description
+                imageView.load(movieDetail.poster)
 
-           supportFragmentManager.beginTransaction()
-               .replace(R.id.fragment_top_container,topFragment)
-               .addToBackStack(null)
-               .commit()
+                // Carga el TopFragment con título e imagen usando newInstance
+                val topFragment = TopFragment.newInstance(movieDetail.title, movieDetail.poster)
+                supportFragmentManager.beginTransaction()
+                    .replace(R.id.fragment_top_container, topFragment)
+                    .addToBackStack(null)
+                    .commit()
 
-           // Carga el BottomFragment con la descripción usando newInstance
-           val bottomFragment = BottomFragment.newInstance(film.info)
-           supportFragmentManager.beginTransaction()
-               .replace(R.id.fragment_bottom_container,bottomFragment)
-               .addToBackStack(null)
-               .commit()
-       }else{
-           Log.e("DetailActivity","Film object is null!")
-       }
+                // Carga el BottomFragment con la descripción usando newInstance
+                val bottomFragment = BottomFragment.newInstance(movieDetail.description)
+                supportFragmentManager.beginTransaction()
+                    .replace(R.id.fragment_bottom_container, bottomFragment)
+                    .addToBackStack(null)
+                    .commit()
 
+                // Manejar el botón para agregar a favoritos
+                button2.setOnClickListener {
+                    addFavoriteMovie(movieDetail)
+                }
+
+            } catch (e: Exception) {
+                Log.e("DetailActivity", "Error en la solicitud de detalles de la película", e)
+            }
+        }
+    }
+
+    private fun addFavoriteMovie(movieDetail: MovieDetailDataModel) {
+        lifecycleScope.launch {
+            val userId = sharedPreferences2.getInt("userId", -1)
+            if (userId != -1) {
+                val favoriteMovie = FavoriteMovieEntity(
+                    name = movieDetail.title,
+                    year = movieDetail.releaseDate.substring(0, 4),
+                    poster = movieDetail.poster,
+                    timeDuraction = "${movieDetail.runtime} minutos"
+                )
+                Log.d("DetailActivity", "Adding favorite movie: ${favoriteMovie.movieId}")
+                userRepositoryViewModel.addMovieToFavorites(userId, favoriteMovie)
+            } else {
+                Log.e("DetailActivity", "User ID not found in shared preferences!")
+            }
+        }
     }
 }
